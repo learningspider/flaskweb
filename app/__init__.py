301a690753a4c46c068dfec489609e6f754688b5
learningspider <1161192890@qq.com>
2018-03-20 20:15:31 +0800

    create environment

--- app/__init__.py
+++ app/__init__.py
@@ -28,10 +28,6 @@ def create_app(config_name):
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
-    from .auth import auth as auth_blueprint
-    app.register_blueprint(auth_blueprint,url_prefix='/auth')
 
-    from .admin import admin as admin_blueprint
-    app.register_blueprint(admin_blueprint,url_prefix='/admin')
 
     return app
